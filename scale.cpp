4345beee3457dc4c32648785459f9074405b4cb4
Taras Dzedzey <T.Dzedzey@activevideo.com>
2018-01-17 15:12:26 +0000

    optimisation: faster, less operation, do not forget destroy

--- scale.cpp
+++ scale.cpp
@@ -4,24 +4,19 @@
 
 int main()
 {
-  cairo_surface_t *scaleSF = cairo_image_surface_create_from_png ("1.png");
+  cairo_surface_t *source = cairo_image_surface_create_from_png ("1.png");
+  cairo_surface_t *dest = cairo_surface_create_similar(source, cairo_surface_get_content(source), cairo_image_surface_get_width(source) / 2, cairo_image_surface_get_height(source) / 2);
 
 
-  cairo_t *context = cairo_create (scaleSF);
+  cairo_t *context = cairo_create(dest);
   cairo_scale(context, 0.5f, 0.5f);
-  cairo_set_source_surface(context, scaleSF, 0,0);
-  cairo_pattern_set_extend (cairo_get_source(context), CAIRO_EXTEND_NONE);
-  cairo_set_operator (context, CAIRO_OPERATOR_SOURCE);
+  cairo_set_source_surface(context, source, 0 , 0);
+//  cairo_pattern_set_extend (cairo_get_source(context), CAIRO_EXTEND_NONE);
+//  cairo_set_operator (context, CAIRO_OPERATOR_SOURCE);  
+//cairo_pattern_set_filter(cairo_get_source(context), CAIRO_FILTER_NEAREST);
   cairo_paint(context);
-  cairo_surface_t *scaledSF = cairo_surface_create_similar(scaleSF, 
-                                          cairo_surface_get_content (scaleSF), 
-                                          static_cast<int>(cairo_image_surface_get_width(scaleSF)*0.5f), 
-                                          static_cast<int>(cairo_image_surface_get_height(scaleSF)*0.5f));
-  cairo_t *contextScaled = cairo_create(scaledSF);
-  cairo_set_source_surface(contextScaled, scaleSF, 0,0);
-  cairo_paint(contextScaled);
 
-  cairo_status_t cairoStatus = cairo_surface_write_to_png (scaledSF, "scaled.png");
+  cairo_status_t cairoStatus = cairo_surface_write_to_png (dest, "scaled.png");
   if(cairoStatus) std::cout << "shit\n";
   cairo_destroy(context);
   return 0;
